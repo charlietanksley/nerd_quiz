1d74e98668a4dd6c9fe5a688094cc25459e95db9
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-09-07 22:03:46 -0400

    getting things started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NerdQuiz::Application.config.secret_key_base = '94447f85bc2d8eedb203ccf70f19aa3d795862d0ea1c58b006b0c9bc50cc1311f335a749379b5861387b1c5691047f4806b0c174d8eea3956d2eb83e4d08f2d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NerdQuiz::Application.config.session_store :cookie_store, key: '_nerd_quiz_session'
