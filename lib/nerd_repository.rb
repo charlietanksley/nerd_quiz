6d69c6df0721c70c73e61d580b42a5efbafb00d5
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-09-27 23:48:03 -0400

    simplify nerd repository interface

--- lib/nerd_repository.rb
+++ lib/nerd_repository.rb
@@ -2,14 +2,6 @@ require 'oauth2'
 
 NerdRepository = Struct.new(:access_token) do
   def all
-    @all ||= access_token.get('users.json')
-  end
-
-  def to_a
-    all.parse
-  end
-
-  def to_json
-    all
+    access_token.get('users.json').parsed
   end
 end
