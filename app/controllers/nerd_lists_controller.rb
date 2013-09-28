777a2553bb09c96c0ec350d0c4d4ec49ebf7a52f
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-09-27 23:14:46 -0400

    add a nerds show endpoint

---
+++ app/controllers/nerd_lists_controller.rb
@@ -0,0 +1,15 @@
+require Rails.root.join('lib', 'nerd_repository')
+require Rails.root.join('lib', 'access_token_builder')
+
+class NerdListsController < ApplicationController
+  def show
+  end
+
+  private
+
+  def nerds
+    access_token = AccessTokenBuilder.build(current_user)
+    NerdRepository.new(access_token).all
+  end
+  helper_method :nerds
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NerdQuiz::Application.routes.draw do
   root 'home#index'
   get '/auth/stable/callback/', to: 'sessions#create'
+
+  resource :nerd_list, only: [:show]
 end
