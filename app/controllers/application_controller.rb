297a96ed33dbc7fdf320c1cb2b8f8dbe4fdf0cd5
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-09-27 23:36:31 -0400

    authenticate all our requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,29 @@
 class ApplicationController < ActionController::Base
+  NoCurrentUser = Module.new
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   private
 
+  def authenticate!
+    current_user
+  rescue NoCurrentUser
+    redirect_to new_session_path
+  end
+  helper_method :authenticate!
+
   def current_user
     @current_user ||= User.find(session[:user_id].to_s)
-  rescue
-    NullUser.new
+  rescue ActiveRecord::RecordNotFound => e
+    e.extend(NoCurrentUser)
+    raise
   end
   helper_method :current_user
 
   def current_user=(user)
     session[:user_id] = user.id
     @current_user = user
-  end
-
-  class NullUser
-    def name
-      ''
-    end
   end
 end
