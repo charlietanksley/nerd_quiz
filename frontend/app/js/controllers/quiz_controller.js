5c782621d638eec261276853d8120b3635d6960c
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-11 15:18:22 -0500

    start getting layout right using 'live' data
    Where 'live' means static-but-from-the-controller.

---
+++ frontend/app/js/controllers/quiz_controller.js
@@ -0,0 +1,12 @@
+App.QuizController = Ember.ObjectController.extend({
+  answerImage: "https://gravatar.com/avatar/60f48400a3ff3899b2125f28657fab62?class=img-polaroid&d=mm&size=450"
+
+, answerChoices: [
+  [{name: "Some Nerdname"}
+  , {name: "Aaron Hillegass"}
+  ]
+, [{name: "CBQ"}
+  , {name: "Somenerd Longname-Longname"}
+  ]
+]
+});
