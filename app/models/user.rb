568735ea80e953d4cdfaf06da2b5b09df5aae4a0
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-13 11:58:50 -0500

    add a few authenticate methods to User
    We need to make authenticating a user as direct as possible.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_one :oauth_token, inverse_of: :user, dependent: :destroy
+
+  def self.authenticate(api_token)
+    joins(:oauth_token)
+      .merge(OauthToken.where(token: api_token))
+      .first
+  end
+
+  def api_token
+    oauth_token.token
+  end
 end
