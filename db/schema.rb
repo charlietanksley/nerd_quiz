c02659e06788db3bf545afb4cf3f4a25246fba7f
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-11-13 11:39:48 -0500

    schema change migrating forced on me
    I don't know what this is.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130928015638) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "oauth_tokens", force: true do |t|
     t.string   "token",         null: false
     t.string   "refresh_token", null: false
